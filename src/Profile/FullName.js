d49790766ad0ec83b3c5066891991865e8ceb694
Slim-us <66728561+Slim-us@users.noreply.github.com>
2020-08-26 10:51:46 +0100

    Add files via upload

---
+++ src/Profile/FullName.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../App.css' ;
+
+
+
+const FullName = ()=> {
+    return (
+      <div className="FullName">
+          <h1>My name: Slim Bouchiba</h1>
+        
+        
+          
+        
+      </div>
+    );
+  };
+  
+  export default FullName;
